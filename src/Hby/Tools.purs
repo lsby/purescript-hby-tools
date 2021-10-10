9c8b79a5725de6143daae5320155cf7cccf78f3b
hbybyyang <2451759073@qq.com>
2021-10-10 10:25:57 +0800

    深层的map和ap方法

--- src/Hby/Tools.purs
+++ src/Hby/Tools.purs
@@ -21,3 +21,16 @@ changeLeft :: forall a b c. (a -> b) -> Either a c -> Either b c
 changeLeft fn a = case a of
   Left err -> Left $ fn err
   Right d -> Right d
+
+map_l2 :: forall m1 m2 a b. Functor m1 => Functor m2 => (a -> b) -> m1 (m2 a) -> m1 (m2 b)
+map_l2 = map <<< map
+
+infixl 4 map_l2 as <$$>
+
+apply_l2 :: forall m1 m2 a b. Bind m1 => Apply m2 => m1 (m2 (a -> b)) -> m1 (m2 a) -> m1 (m2 b)
+apply_l2 f v = join $ map (\vn -> map (\fn -> apply fn vn) f) v
+
+infixl 4 apply_l2 as <**>
+
+lift2_l2 :: forall m1 m2 a b c. Bind m1 => Apply m2 => (a -> b -> c) -> m1 (m2 a) -> m1 (m2 b) -> m1 (m2 c)
+lift2_l2 f v1 v2 = f <$$> v1 <**> v2
